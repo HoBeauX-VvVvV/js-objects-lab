8959a9bdd4f03c366345b87f98c313afb6291396
BeauX <fire-fly_7@hotmail.com>
2024-05-15 16:07:13 -0700

    Ex 17 commit

--- app.js
+++ app.js
@@ -272,3 +272,28 @@ game.partyCount = () => {
 }
 
 game.partyCount();
+
+/*
+Exercise 16
+1. Now, complete gyms with a difficulty below 8. Reflect on how this is 
+similar to or different from the previous gym exercises.
+(change the value of `complete` in the qualifying objects from false to true).
+Solve Exercise 16 here:
+*/
+
+game.gyms.forEach((gym) => {
+  if (gym.difficulty < 8) {
+      gym.completed = true;
+  }
+});
+
+console.log(game.gyms);
+
+/*
+Exercise 17
+1. Log the entire `game` object to the console. Take a moment to review the 
+changes you've made throughout the exercises.
+Solve Exercise 17 here:
+*/
+
+console.log(game);
